6da166fdb05480c46146337e2b161b0313cda5b4
medievalStone <tayalanmol1@gmail.com>
2020-12-03 22:38:43 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-   return <div>
-      <Homepage />
+const  App= () =>(
+   <div>
+      <Switch>
+      <Route path='/' component= {Homepage} />
+      </Switch>
     </div>
-}
+)
 
 export default App;
