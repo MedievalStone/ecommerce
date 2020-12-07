5f2e77794dd4a5080618c11f0760517dce74d07c
medievalStone <tayalanmol1@gmail.com>
2020-12-07 14:32:32 +0530

    Collection preview and item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import React from 'react';
 import {Homepage} from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage  from './pages/shop/shop.component';
+
+const HatsPage = () => (
+   <div>
+      <h1>Welcome to the hats Page</h1>
+   </div>
+)
 
 const  App= () =>(
    <div>
       <Switch>
-      <Route path='/' component= {Homepage} />
+         <Route exact path='/' component= {Homepage} />
+         <Route path='/shop' component={ShopPage} />
+         <Route path='/shop/hats' component={HatsPage} />
       </Switch>
     </div>
 )
